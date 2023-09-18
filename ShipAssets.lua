7822cd52c1e043af99961cbb1776402e116ed24f
Steven <141743202+StxvxnsGithub@users.noreply.github.com>
2023-09-18 11:50:38 +0100

    Create ShipAssets.lua

---
+++ ShipAssets.lua
@@ -0,0 +1,64 @@
+local ShipAssets = {
+	
+	-- Royal Navy --
+	
+	["Queen Elizabeth-class"] = {
+		["Illustrious"] 	= {"L","","R10","ILLUSTRIOUS","rbxassetid://14043238073"},
+		["Prince of Wales"] = {"P","","R09","PRINCE OF WALES",""},
+	},
+	["Albion-class"] = {
+		["Albion"] 	= "rbxassetid://14043238073",
+		["Prince of Wales"] = ""
+	},
+	["Daring-class"] = {
+		["York"] 			= {"Y","K","D90","YORK","rbxassetid://6295382187"},
+		["Somerset"] 		= {"S","M","D94","SOMERSET","rbxassetid://6680886211"},
+		
+		["Delta"] 			= {"D","L","D36","DELTA","rbxassetid://"},
+		["Defiance"] 		= {"D","C","D37","DEFIANCE","rbxassetid://"},
+		["Discovery"] 		= {"D","S","D40","DISCOVERY","rbxassetid://"},
+		["Diadem"] 			= {"D","D","D42","DIADEM","rbxassetid://"}
+	},
+	["Duke-class"] = {
+		["Nelson"] 			= {"N","S","F230","NELSON","rbxassetid://4674487023"},
+		["Drake"] 			= {"D","K","F231","DRAKE","rbxassetid://6043219230"},
+		
+		["Lancaster"] 		= {"D","K","F229","LANCASTER","rbxassetid://"},
+		["Winchester"] 		= {"W","C","F235","WINCHESTER","rbxassetid://"},
+		["Montrose"]	 	= {"M","R","F236","MONTROSE","rbxassetid://"},
+		["Cambridge"] 		= {"C","B","F237","CAMBRIDGE","rbxassetid://"},
+		["London"] 			= {"L","D","F238","LONDON","rbxassetid://"},
+		["Gloucester"] 		= {"G","C","F239","GLOUCESTER","rbxassetid://"},
+		
+		["Halifax"] 		= {"H","F","F240","HALIFAX","rbxassetid://"}
+	},
+	["River-class Batch One"] = {
+		["Tyne"] 			= {"","","P281","TYNE","rbxassetid://"},
+		["Severn"] 			= {"","","P282","SEVERN","rbxassetid://"},
+		["Mersey"] 			= {"","","P283","MERSEY","rbxassetid://1401174182"}
+	},
+	["Sandown-class"] = {
+		["Penzance"] 		= {"","","M220","PENZANCE","rbxassetid://6028493159"}
+	},
+	["Archer-class"] = {
+		["Biter"] 			= {"","","P270","BITER","rbxassetid://6189501636"},
+		["Sabre"] 			= {"","","P285","SABRE","rbxassetid://4448589441"}
+	},
+	
+	-- Royal Fleet Auxiliary --
+	
+	["Bay-class"] = {
+		["Roughneck"] 		= {"R","K","L3007","ROUGHNECK","rbxassetid://"},
+		
+		["Mounts Bay"] 		= {"M","B","L3008","MOUNTS BAY","rbxassetid://7045205548"},
+		["Stokes Bays"] 	= {"S","B","L3009","STOKES BAY","rbxassetid://"}
+	},
+	["Tide-class"] = {
+		["Westmorland"] 	= {"W","L","A126","WESTMORLAND","rbxassetid://6702693256"},
+		
+		["Tidepool"] 		= {"T","P","A134","TIDEPOOL","rbxassetid://"},
+		["Tidemark"] 		= {"T","M","A135","TIDEMARK","rbxassetid://"}
+	}
+}
+
+return ShipAssets
